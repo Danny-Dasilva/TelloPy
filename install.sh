48f15b1a961916a4fea3ba642971df8360bc3943
Danny-Dasilva <yahchayildasilva@gmail.com>
2019-09-14 15:05:49 -0400

    added av

--- install.sh
+++ install.sh
@@ -2,3 +2,12 @@ sudo apt-get install python3-pip
 sudo pip3 install tellopy
 sudo pip3 install pygame
 sudo apt-get install mplayer
+
+
+sudo apt-get install -y python-dev pkg-config
+
+sudo apt-get install -y
+libavformat-dev libavcodec-dev libavdevice-dev
+libavutil-dev libswscale-dev libavresample-dev libavfilter-dev
+
+pip3 install av
