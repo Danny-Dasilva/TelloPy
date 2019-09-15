8487f220793bc36d9a4cfa568d953af6d218aca9
Danny-Dasilva <yahchayildasilva@gmail.com>
2019-09-15 11:04:38 -0400

    moved files

--- tellopy/examples/simple.py
+++ tellopy/examples/simple.py
@@ -1,36 +1,17 @@
 import tellopy
 import av
 from time import sleep
+from handler import handler
 
 
-
-prev_flight_data = None
-
-
-def handler(event, sender, data, **args):
-    global prev_flight_data
-    global flight_data
-    global log_data
-    drone = sender
-    if event is drone.EVENT_FLIGHT_DATA:
-        if prev_flight_data != str(data):
-            print(data)
-            prev_flight_data = str(data)
-        flight_data = data
-    elif event is drone.EVENT_LOG_DATA:
-        log_data = data
-    else:
-        print('event="%s" data=%s' % (event.getname(), str(data)))
-
-
-
-
+# setup
 drone = tellopy.Tello()
 drone.connect()
 drone.subscribe(drone.EVENT_FLIGHT_DATA, handler)
 drone.subscribe(drone.EVENT_LOG_DATA, handler)
 
-sleep(3)
+# need sleep at the beginning to wait for command
+sleep(2)
 
 drone.takeoff()
 sleep(2)
