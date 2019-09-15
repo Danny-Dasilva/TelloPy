ceddbdcd90276f09902cd5de8ae04b8f8e64fc45
Danny-Dasilva <yahchayildasilva@gmail.com>
2019-09-15 14:26:58 -0400

    removed simple

--- install.sh
+++ install.sh
@@ -5,12 +5,8 @@ sudo apt-get install python3-pip
 sudo pip3 install tellopy
 sudo pip3 install pygame
 sudo apt-get install mplayer
-
-
 sudo apt-get install -y python-dev pkg-config
-
 sudo apt-get install -y libavformat-dev libavcodec-dev libavdevice-dev libavutil-dev libswscale-dev libavresample-dev libavfilter-dev
-
 pip3 install av
 # sudo apt install build-essential cmake git libgtk2.0-dev pkg-config libavcodec-dev libavformat-dev libswscale-dev
 # # Dependencies for Python bindings
